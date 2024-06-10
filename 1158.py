e1b0dbe3a467ba5b99752c55460eeaaf60ee4be0
dmcheon <dmcheon@gmail.com>
2024-06-10 17:40:05 -0400

    git stash error fix

--- 1158.py
+++ 1158.py
@@ -2,7 +2,7 @@ N, K = map(int, input().split())
 
 origin = [i + 1 for i in range(N)]
 result = []
-index = 0
+pop_index = 0
 for i in range(N):
     pop_index = (pop_index + (K - 1)) % len(origin)
     result.append(origin.pop(pop_index))
