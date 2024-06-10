0879221902a8df47041f5979cf4989f23122e3ce
dmcheon <dmcheon@gmail.com>
2024-06-10 17:31:46 -0400

    [fix] git stash wasn't proper. This is correct ver.

--- 1158.py
+++ 1158.py
@@ -1,17 +1,11 @@
-# import sys
-
-# input = sys.stdin.readline().split()
-# N = int(input[0])
-# K = int(input[1])
-
 N, K = map(int, input().split())
 
 origin = [i + 1 for i in range(N)]
 result = []
 index = 0
 for i in range(N):
-    pop_index = (index + (K - 1)) % len(origin)
+    pop_index = (pop_index + (K - 1)) % len(origin)
     result.append(origin.pop(pop_index))
-    index = pop_index
+ 
         
 print(str(result).replace('[', '<').replace(']', '>'))
