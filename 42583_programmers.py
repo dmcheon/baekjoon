79c7633c1ae955758aa605b1b00be949ab24fd33
dmcheon <dmcheon@gmail.com>
2024-06-11 17:26:37 -0400

    fix name

--- 42583_programmers.py
+++ 42583_programmers.py
@@ -8,9 +8,9 @@ def solution(bridge_length, weight, truck_weights):
     while truck_weights:
         answer += 1
         time_bridge = [x + 1 for x in time_bridge]
-        copy_time_truck_on_bridge = copy.deepcopy(time_bridge)
+        copy_time_bridge = copy.deepcopy(time_bridge)
         
-        for i, time in enumerate(copy_time_truck_on_bridge):
+        for i, time in enumerate(copy_time_bridge):
             if time > bridge_length:
                 time_bridge.pop(i)
                 bridge.pop(i)
